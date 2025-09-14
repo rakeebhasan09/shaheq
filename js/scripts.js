7812b091398a44674946780b1c9658aade8ed3ec
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-09-14 16:32:29 +0600

    Desktop 18 page complete with responsiveness........

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 (function ($) {
-	$(document).ready(function () {});
+	$(document).ready(function () {
+		// Password Toggler
+		jQuery(".password i").click(function () {
+			let typeValue = jQuery("#password").attr("type");
+			if (typeValue === "password") {
+				jQuery("#password").attr("type", "text");
+				jQuery(this).addClass("fa-eye-slash");
+			} else {
+				jQuery("#password").attr("type", "password");
+				jQuery(this).removeClass("fa-eye-slash");
+			}
+		});
+		jQuery(".re_password i").click(function () {
+			let typeValue = jQuery("#re_password").attr("type");
+			if (typeValue === "password") {
+				jQuery("#re_password").attr("type", "text");
+				jQuery(this).addClass("fa-eye-slash");
+			} else {
+				jQuery("#re_password").attr("type", "password");
+				jQuery(this).removeClass("fa-eye-slash");
+			}
+		});
+	});
 })(jQuery);
